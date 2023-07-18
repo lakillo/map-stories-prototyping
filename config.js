df91a7a17747164dae912bfdc06fce75afea27a0
Lucy Killoran <l.killoran.1@research.gla.ac.uk>
2023-07-18 15:52:57 +0100

    change text test

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ var config = {
     useCustomLayers: false, //set true for enabling custom layers from sources.js
     bookmarks: true,
     chapterReturn: true,
-    title: 'The Title Text of this Story',
+    title: 'map stories prototyping',
     logo: '',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    subtitle: 'prototyping space data visualisation walkthroughs for accessbility.',
+    byline: 'by Lucy K',
     mobileview: '<div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">', // to add custom messaging in the header for mobile devices
     footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> template.',
     chapters: [
@@ -20,13 +20,13 @@ var config = {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'explaining data types',
             image: './path/to/image/source.png',
             caption: 'Source image caption',
             website: '<a href="" target="_blank">Website</a>',
-            author: 'Digital Storyteller',
+            author: 'Lucy K',
             legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'Text here.',
             location: {
                 center: [35, 1],
                 zoom: 3.5,
